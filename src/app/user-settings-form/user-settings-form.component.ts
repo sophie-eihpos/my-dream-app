98f86d7ff48d8d6ad18a3bbbdaef78b105c82a96
Sophie Jiang <sifeij@gmail.com>
2023-09-25 10:20:18 -0700

    add data bidining in angular forms

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
-export class UserSettingsFormComponent {
+export class UserSettingsFormComponent implements OnInit {
 
+  originalUserSettings: UserSettings = {
+    name: 'milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
+
+  userSettings: UserSettings ={...this.originalUserSettings};
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
 }
